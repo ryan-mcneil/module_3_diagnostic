b787bac102d22d080f1167e975c550b2c894a903
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-12-12 10:07:39 -0700

    get response

--- app/facades/station_facade.rb
+++ app/facades/station_facade.rb
@@ -1,10 +1,16 @@
 class StationFacade
 
   def initialize(zip)
-
+    @zip = zip.to_s
   end
 
   def stations
-
+    response = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json") do |f|
+      f.headers['api-key'] = ENV['NREL_API_KEY']
+      f.headers['fuel_type'] = 'LPG,ELEC'
+      f.headers['location'] = @zip
+      f.adapter  Faraday.default_adapter
+    end
+    binding.pry
   end
 end
